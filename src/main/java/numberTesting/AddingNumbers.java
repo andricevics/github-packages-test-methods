69ed2c2e6f418c8d529d33308e7dc986e0a76396
andricevics <stjepan.andricevic@less.hr>
2021-10-12 09:21:57 +0200

    Frist commit

---
+++ src/main/java/numberTesting/AddingNumbers.java
@@ -0,0 +1,12 @@
+package numberTesting;
+
+public class AddingNumbers {
+
+    public double addTwoNumbers(double num1, double num2){
+        return num1 + num2;
+    }
+
+    public double addThreeNumbers(double num1, double num2, double num3){
+        return num1+ num2 + num3;
+    }
+}
